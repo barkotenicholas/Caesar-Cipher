f40c72b48f3d13f3537f234e8d67fb7d19f3bf5d
barkotenicholas <barkotenicholas@gmail.com>
2022-02-17 16:14:37 +0300

    Added first test

--- src/test/java/DecodeTest.java
+++ src/test/java/DecodeTest.java
@@ -1,5 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DecodeTest {
+
+    Decode decode;
+    @BeforeEach
+    public void setUp(){
+        decode = new Decode();
+    }
+
+    @Test
+    @DisplayName("Encoded class should instantiate correctly")
+    public void newDecoded_instantiatesCorrectly(){
+        assertNotNull(decode);
+    }
 
 }
