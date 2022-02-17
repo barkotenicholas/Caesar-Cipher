28143c446c45ae4c0a0c6483b34b5bcbf269acb0
barkotenicholas <barkotenicholas@gmail.com>
2022-02-17 17:13:46 +0300

    Added logic in decoded class

--- src/main/java/Decode.java
+++ src/main/java/Decode.java
@@ -1,2 +1,45 @@
+import constants.Consts;
+
+import java.util.Locale;
+
 public class Decode {
+
+    private int shiftkey;
+    private String cypherString;
+    String cypher;
+    public Decode(String cypherString,int shiftKey) {
+        this.shiftkey = shiftKey;
+        this.cypherString = cypherString;
+        cypher = "";
+    }
+
+    public String decode(){
+        StringBuilder stringBuilder = new StringBuilder();
+        cypherString = cypherString.toLowerCase(Locale.ROOT);
+
+        for (int i = 0; i< cypherString.length();i++){
+
+            int position = Consts.ALPHABET.indexOf(cypherString.charAt(i));
+            int val =(position - shiftkey) %26;
+
+            if (val < 0)
+                val = Consts.ALPHABET.length() + val;
+
+            char replace = Consts.ALPHABET.charAt(val);
+            stringBuilder.append(replace);
+        }
+        cypher = stringBuilder.toString();
+
+        return cypher;
+
+    }
+
+    public int getShiftKey() {
+        return shiftkey;
+    }
+
+
+    public String getCypherString() {
+        return cypherString;
+    }
 }
--- src/test/java/DecodeTest.java
+++ src/test/java/DecodeTest.java
@@ -9,13 +9,34 @@ class DecodeTest {
     Decode decode;
     @BeforeEach
     public void setUp(){
-        decode = new Decode();
+        decode = new Decode("snhmtqfx",5);
     }
 
     @Test
     @DisplayName("Encoded class should instantiate correctly")
     public void newDecoded_instantiatesCorrectly(){
         assertNotNull(decode);
+    }
+
+    @Test
+    @DisplayName("Decode class should get shiftKey")
+    public void newEncoded_getShiftKey_5(){
+        assertEquals(5,decode.getShiftKey());
+    }
+
+
+    @Test
+    @DisplayName("Decoded class should get Cypher String")
+    public void newDecoded_getCypherString_Cypher(){
+        assertEquals("snhmtqfx",decode.getCypherString());
+    }
+
+    @Test
+    @DisplayName("Decoded class should Decipher Input String")
+    public void newDecoded_shiftInputString_Cypher(){
+
+        assertEquals("nicholas",decode.decode());
+
     }
 
 }
