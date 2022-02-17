4418d8708172c2391dfc16ad2d3937145b427527
barkotenicholas <barkotenicholas@gmail.com>
2022-02-17 14:51:55 +0300

    First test to ensure encoded class instantiates correctly

--- src/test/java/EncodeTest.java
+++ src/test/java/EncodeTest.java
@@ -1,5 +1,22 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EncodeTest {
+    
+    Encode encode;
+    
+    @BeforeEach
+    public void setUp(){
+        encode = new Encode();
+    }
 
+    @Test
+    @DisplayName("Encoded class should instantiate correctly")
+    void newEncoded_instantiatesCorrectly() {
+        assertNotNull(encode);
+    }
 }
