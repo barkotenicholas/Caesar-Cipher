5a2fe57488c37fc1b19223707616804dc5a9910f
barkotenicholas <barkotenicholas@gmail.com>
2022-02-17 18:55:21 +0300

    Addedd gui

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,21 +6,22 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the Text you want to be Encrypted :");
         String inputString = scanner.nextLine();
-        System.out.println("Enter the Shift Key :");
+        System.out.println("Enter the Shift Key : (1-25)");
         int shiftkey = scanner.nextInt();
 
-        Encode encode ;
-        Decode decode;
-        encode = new Encode(inputString,shiftkey);
-        decode = new Decode(encode.getEncodedText(), shiftkey);
+        if (shiftkey >= 1 && shiftkey <= 25) {
 
-        System.out.println("Input Message" + inputString);
-        System.out.println("Encrypted Message is \n"+encode.encode());
-        System.out.println("Decrypted Message is \n"+decode.decode());
+            Encode encode;
+            Decode decode;
+            encode = new Encode(inputString, shiftkey);
+            System.out.println("Input Message :" + inputString);
+            System.out.println("Encrypted Message is \n" + encode.encode());
 
-
-
-
+            decode = new Decode(encode.getEncodedText(), shiftkey);
+            System.out.println("Decrypted Message is \n" + decode.decode());
+        }else {
+            System.out.println("Please input a shift key between 1 and 25");
+        }
 
 
     }
